e0b29bd408f84b753e5e2aa1405bd326e4f5ea3f
asadiqi <asadiqi@helb-prigogine.be>
2025-08-05 23:25:43 +0200

    Creation de la classe Assassin

--- demo/src/main/java/com/example/demo/units/Assassin.java
+++ demo/src/main/java/com/example/demo/units/Assassin.java
@@ -1,8 +1,28 @@
 package com.example.demo.units;
 
-public class Assassin {
+import com.example.demo.City;
+import com.example.demo.GameElement;
+
+public class Assassin extends Unit {
 
 
-    public static String northCollecterPath = "img/white/northAssassin.png";
-    public static String southCollecterPath = "img/black/southAssassin.png";
+    public static String northAssassinPath = "img/white/northAssassin.png";
+    public static String southAssassinPath = "img/black/southAssassin.png";
+
+    private City city; // dans Collecter
+
+    public Assassin(GameElement position, City city) {
+        super(position);
+        this.city = city;
+    }
+
+    @Override
+    public String getImagePath() {
+        return city.isNorth ? northAssassinPath : southAssassinPath;
+    }
+
+
+
 }
+
+
