e553b749ade6689d3e76aefc2086fb43fb99ef42
asadiqi <asadiqi@helb-prigogine.be>
2025-08-14 23:03:46 +0200

    MagicStone commentaire

--- demo/src/main/java/com/example/demo/collectable/MagicStone.java
+++ demo/src/main/java/com/example/demo/collectable/MagicStone.java
@@ -5,22 +5,28 @@ import java.util.List;
 
 public class MagicStone extends GameElement {
 
-    public static String magicStoneImagePath = "img/magicStone.png";
+    public static String magicStoneImagePath = "img/magicStone.png";// chemin de l'image magicstone
 
+    // Constructeur de la classe MagisStone
     public MagicStone(GameElement position) {
         super(position.getX(), position.getY());
 
     }
 
+    // retourne image de la pierre magique
     @Override
     public String getImagePath() {
         return magicStoneImagePath;
     }
 
+    // Crée une pierre magique sur une cellule libre aléatoire
+    // allElements : tous les éléments de la grille
+    // gridRows, gridCols : taille de la grille
     public static void createMagicStoneAtRandomPosition(List<GameElement> allElements, int gridRows, int gridCols) {
+        // trouve une cellule libre
         GameElement freePos = GameElement.getRandomFreeCell(gridCols, gridRows, allElements);
 
-        // Vérifie si la position contient déjà un Flag
+        // vérifie qu'il n'y a pas de drapeau sur cette position
         for (GameElement e : allElements) {
             if (e.getX() == freePos.getX() && e.getY() == freePos.getY() && e instanceof Flag) {
                 System.out.println("MagicStone non créé : case occupée par un Flag.");
@@ -28,6 +34,7 @@ public class MagicStone extends GameElement {
             }
         }
 
+        // ajoute la pierre magique si position valide
         if (!freePos.equals(GameElement.NO_POSITION)) {
             allElements.add(new MagicStone(freePos));
         } else {
